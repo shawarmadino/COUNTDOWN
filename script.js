f1f4b3f93393aa220456f57a07ebfbb1f3dd04d3
shawarmadino <gino.cleanslate@gmail.com>
2025-01-01 22:43:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,9 +27,6 @@
             content.style.display = "block";
   
 
-window.location = “https://shawarmadino.github.io/HB/”;
-else
-window.location = “https://shawarmadino.github.io/HB/”;
             clearInterval(x);
           
           //seconds
