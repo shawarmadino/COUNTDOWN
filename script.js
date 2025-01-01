30217850f11e1c7096eec3055a6927fc0e27fcc6
shawarmadino <gino.cleanslate@gmail.com>
2025-01-01 23:07:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
           hour = minute * 60,
           day = hour * 24;
   
-    let birthday = "Jan 02, 2025 00:00:30",
+    let birthday = "Jan 02, 2025 00:00:45",
         countDown = new Date(birthday).getTime(),
         x = setInterval(function() {    
   
